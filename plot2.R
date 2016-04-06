0326d48aa3639f0d7deffb08f2983caac701e74c
wroka <wroka@upenn.edu>
2016-04-06 12:24:40 -0400

    Second commit

--- plot2.R
+++ plot2.R
@@ -1,16 +1,19 @@
 # plot2
+library(lubridate)
+library(dplyr)
 ## download data file
 URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(URL, destfile="./Dataset.zip")
 ## Unzip files to working folder
 unzip(zipfile="./Dataset.zip")
 ## read txt file into table 
-data_temp <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE, dec = ".")
+data_temp <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE, na.strings = "?", dec = ".")
 ## subset to only include 2/1/2007 and 2/2/2007
 plot2 <- subset(data_temp, Date %in% c("1/2/2007","2/2/2007"))
 #str(plot2)
-# day is combined date and time as x-axis
-day <- strptime(paste(plot2$Date, plot2$Time, sep=" "), "%d/%m/%Y %H:%M:%S") # paste converts to character, strptime to POSIXlt
+# add day column - combined datetime
+plot2 <- mutate(plot2, day = parse_date_time(paste(Date,Time), "dmyHMS"))
+str(plot2)        
 ## create plot
 png("plot2.png", width=480, height=480)
 #plot(day, gap, type="l", xlab="", ylab="Global Active Power (kilowatts)")
