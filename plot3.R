04211c62d9963fb96b9dea1a7f97447eac931e43
wroka <wroka@upenn.edu>
2016-04-06 12:23:47 -0400

    2nd Commit

--- plot3.R
+++ plot3.R
@@ -1,15 +1,17 @@
 # plot3
+library(lubridate)
+library(dplyr)
 ## download data file
 URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(URL, destfile="./Dataset.zip")
 ## Unzip files to working folder
 unzip(zipfile="./Dataset.zip")
 ## read txt file into table 
-data_temp <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE, dec = ".")
+data_temp <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE, na.strings = "?", dec = ".")
 ## subset to only include 2/1/2007 and 2/2/2007
 plot3 <- subset(data_temp, Date %in% c("1/2/2007","2/2/2007"))
 # day is combined date and time as x-axis
-day <- strptime(paste(plot3$Date, plot3$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+plot3 <- mutate(plot3, day = parse_date_time(paste(Date,Time), "dmyHMS"))
 ## create plot
 png("plot3.png", width=480, height=480)
 with(plot3, plot(day, as.numeric(Sub_metering_1), type="l", ylab="Energy Submetering", xlab=""))
